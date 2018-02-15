9e74f2fe52010572225a5d5edc77ed5a8f8671ef
rgab1 <rgab1@LINUX>
2018-02-15 12:12:31 +0530

    settings.py

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('BLOGGY_SECRET')
+SECRET_KEY = 'sh1#jeq44-w8z_0pbwxaj_80e7^-8m3_z$8enjw#5ge#9kubx$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
